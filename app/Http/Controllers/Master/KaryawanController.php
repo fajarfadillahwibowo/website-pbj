ec86d6e2b35adaef9f6056c15bcfd729822f6a8f
fajarfadillahwibowo <fajarfadillahwibowo@users.noreply.github.com>
2026-09-01 14:45:39 +0700

    feat: hubungkan Master Karyawan (semua kategori personil) dan Data Driver (filter supir) secara dinamis & konsisten

--- app/Http/Controllers/Master/KaryawanController.php
+++ app/Http/Controllers/Master/KaryawanController.php
@@ -4,11 +4,54 @@ namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Master\Karyawan;
+use App\Models\Autentikasi\Jabatan;
 
 class KaryawanController extends Controller
 {
-    public function index()
+    /**
+     * Tampilkan seluruh master data karyawan (Staf, Driver, Gudang, Teknisi, Manajemen).
+     */
+    public function index(Request $request)
     {
-        return view('master.karyawan.index');
+        $kataKunci = $request->input('cari');
+        $filterKategori = $request->input('kategori');
+
+        $query = Karyawan::with('jabatan');
+
+        if ($filterKategori && in_array($filterKategori, ['staf', 'driver', 'teknisi', 'gudang', 'manajemen'])) {
+            $query->where('kategori_karyawan', $filterKategori);
+        }
+
+        if ($kataKunci) {
+            $query->where(function ($q) use ($kataKunci) {
+                $q->where('nama_karyawan', 'like', "%{$kataKunci}%")
+                  ->orWhere('kode_karyawan', 'like', "%{$kataKunci}%")
+                  ->orWhere('no_hp', 'like', "%{$kataKunci}%")
+                  ->orWhere('no_identitas', 'like', "%{$kataKunci}%");
+            });
+        }
+
+        $daftarKaryawan = $query->orderBy('kode_karyawan', 'asc')->get();
+        $daftarJabatan = Jabatan::orderBy('id_jabatan', 'asc')->get();
+
+        // Hitung statistik per kategori
+        $totalSemua = Karyawan::count();
+        $totalDriver = Karyawan::where('kategori_karyawan', 'driver')->count();
+        $totalStaf = Karyawan::where('kategori_karyawan', 'staf')->count();
+        $totalGudang = Karyawan::where('kategori_karyawan', 'gudang')->count();
+        $totalTeknisi = Karyawan::where('kategori_karyawan', 'teknisi')->count();
+
+        return view('master.karyawan.index', compact(
+            'daftarKaryawan',
+            'daftarJabatan',
+            'kataKunci',
+            'filterKategori',
+            'totalSemua',
+            'totalDriver',
+            'totalStaf',
+            'totalGudang',
+            'totalTeknisi'
+        ));
     }
 }
